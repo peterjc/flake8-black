d4c60042ac00eef10ccb1bca106daa3aa727c232
peterjc <p.j.a.cock@googlemail.com>
2019-02-15 10:49:22 +0000

    Simple test case where black would make changes

---
+++ tests/test_changes/hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Print 'Hello world' to the terminal.
+
+This is a simple test script using a hashbang line and a PEP263 encoding line.
+
+In the original format, there excess spaces in the print call, and unwanted
+blank lines - which black will remove.
+"""
+
+
+print ( "Hello world" )
+
