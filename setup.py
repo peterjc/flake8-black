e1f4f1c1e2e9adef0ea3424e432720c94e988110
Peter Cock <p.j.a.cock@googlemail.com>
2020-01-28 15:31:05 +0000

    flake8 line length vs black

--- setup.py
+++ setup.py
@@ -23,7 +23,9 @@ setup(
     author_email="p.j.a.cock@googlemail.com",
     url="https://github.com/peterjc/flake8-black",
     project_urls={
-        "Documentation": "https://github.com/peterjc/flake8-black/blob/master/README.rst",
+        "Documentation": (
+            "https://github.com/peterjc/flake8-black/blob/master/README.rst"
+        ),
         "Source": "https://github.com/peterjc/flake8-black/",
         "Tracker": "https://github.com/peterjc/flake8-black/issues",
     },
