dc3f0de705277264821c7ed706f97c47f2c630af
Brian Helba <brian.helba@kitware.com>
2021-07-16 15:37:32 -0400

    Explicitly depend on the same TOML parser as Black
    Black has switched from "toml" to "tomli", so also using "tomli" here should
    ensure that this tool can read the same TOML config files.
    
    Since this does directly import and use the "tomli" package, it should be an
    explicit dependency.
    
    Fixes #32.

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     ],
     keywords="PEP8",
     py_modules=["flake8_black"],
-    install_requires=["flake8 >= 3.0.0", "black", "toml"],
+    install_requires=["flake8 >= 3.0.0", "black", "tomli"],
     entry_points={"flake8.extension": ["BLK = flake8_black:BlackStyleChecker"]},
 )
