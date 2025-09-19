962659088ee0065869fef6fb3f7067e5d9a8c944
Peter J. A. Cock <p.j.a.cock@googlemail.com>
2025-09-19 21:10:53 +0100

    Avoid the flake8 warnings in this test

--- tests/test_changes/black_preview.py
+++ tests/test_changes/black_preview.py
@@ -1,8 +1,17 @@
 """Example showing future black string reformatting."""
 
+a_very_long_variable = 17.3
+short_value = 1234567890
+
+
+def and_a_very_long_function_call():
+    """Deep thought."""
+    return 42
+
+
 my_dict = {
-    "a key in my dict": a_very_long_variable  # noqa: F821
-    * and_a_very_long_function_call()  # noqa: F821
+    "a key in my dict": a_very_long_variable
+    * and_a_very_long_function_call()
     / 100000.0,
-    "another key": (short_value),  # noqa: F821
+    "another key": (short_value),
 }
